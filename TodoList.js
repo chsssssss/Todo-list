424e1100fa645191399f8f1793b026ced46ab060
chsssssss <chaheesun531@naver.com>
2022-05-27 04:26:44 +0900

    todo

--- TodoList.js
+++ TodoList.js
@@ -1,4 +1,8 @@
 document.querySelector('form').addEventListener('submit', addTodo);
+document.querySelector('ul').addEventListener('click', todoManage);
+document.querySelector('#deleteAll').addEventListener('click', deleteAll);
+document.querySelector('#checkbox').addEventListener('change', leftTodo);
+
 // 추가
 function addTodo(e) {
     e.preventDefault();
@@ -11,14 +15,46 @@ function printTodo(newTodo) {
     let ul = document.querySelector('ul');
     let li = document.createElement('li');
     ul.appendChild(li);
-    li.innerHTML = `<input type="checkbox" id="checkbox"><label>${newTodo}</label><a id="delete">X</a>`;
+    li.innerHTML = `<input type="checkbox" id="checkbox"><label id='label'>${newTodo}</label><button type="submit" id="delete">X</button>`;
 }
 
+// 삭제 또는 체크
+function todoManage(e) {
+    if(e.target.id == 'delete') {
+        deleteTodo(e);
+    }
+    else if(e.target.id == 'label'){
+        todoDetail(e);
+    }
+    else{
+        alert('외');
+    }
+}
 
+function todoDetail(e) {
+    let li = document.querySelector('li');
+    let memo = document.createElement('memo'); 
+    li.appendChild(memo);
+    memo.innerHTML = '<input type="text" id="memo">';
+}
 
 // 삭제
-//function deleteTodo()
-
+function deleteTodo(e) {
+    let remove = e.target.parentNode;
+    let parentNode = remove.parentNode;
+    parentNode.removeChild(remove);
+    //let del = e.target.parentElement;
+    //del.remove();
+}
 
 // 전체삭제
-//function deleteAll() 
+function deleteAll(e) {
+    document.querySelector('ul').innerHTML = '';
+}
+
+// 남은 할일
+function leftTodo(e) {
+    let left = document.getElementById('#checkbox');
+    let leftTodo = left.checked;
+    document.getElementById('#left').innerHTML = '<span id="left">{leftTodo}</span>';
+}
